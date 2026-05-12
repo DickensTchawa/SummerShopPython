92dd3d699d1d6c38f8a313644a58665e5528241a
DickensTchawa <114138803+DickensTchawa@users.noreply.github.com>
2026-05-12 13:19:16 +0100

    Update of the Summer Shop
    Mise à jour du projet python

---
+++ Fonctions.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+
+# Calcule le prix total de la vente d'un article
+def vente(pu,qte):
+    v = pu*qte;
+    return v;     
+
+# Calcule la quantité restante après vente d'un article
+def reste(qte,a):
+    s = qte - a;  #Quantité en Stock - Quantité vendu = Quantité restante
+    return s;
+
+#Effectue l'approvisionnenment automatique du stock d'un article après vente(Pas utilisé)
+# def approvisionner(qte,seuil):
+       # while (qte <= seuil):
+            # qte = qte + (2*qte); #Lorsque le seuil est atteint ou dépassé(Stock non vide)
+            # if(qte == 0):
+                # qte = qte + 1000; #Lorsque le stock est vide
+        # return qte;
+
+
